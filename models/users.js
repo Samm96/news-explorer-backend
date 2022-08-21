99b73c0897f4108c6b968555cf5349cf97bb858e
Samm96 <sihorsch96@gmail.com>
2022-08-21 17:39:54 -0400

    Add: prevent password from being returned

--- models/users.js
+++ models/users.js
@@ -1,25 +1,29 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    minLength: [2, 'The EMAIL field length is too short'],
-    maxLength: [30, 'The EMAIL field length is too long'],
-    required: [true, 'Required field'],
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: 'Elise',
+      minLength: [2, 'The NAME field length is too short'],
+      maxLength: [30, 'The NAME field length is too long'],
+      required: [true, 'Required field'],
+    },
 
-  password: {
-    type: String,
-    required: [true, 'Required field'],
-  },
+    email: {
+      type: String,
+      minLength: [2, 'The EMAIL field length is too short'],
+      maxLength: [30, 'The EMAIL field length is too long'],
+      required: [true, 'Required field'],
+    },
 
-  name: {
-    type: String,
-    default: 'Elise',
-    minLength: [2, 'The NAME field length is too short'],
-    maxLength: [30, 'The NAME field length is too long'],
-    required: [true, 'Required field'],
+    password: {
+      type: String,
+      required: [true, 'Required field'],
+      select: false,
+    },
   },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('user', userSchema);
